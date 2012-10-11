1e3983289249b38e93cae1f7c094067d05ce4b43
joudard <joudard@05c4dcad-f92f-4502-bc42-5fbbb5b1d021>
2012-10-11 18:01:53 +0200

    avoid GW side exception on healt-check by reading message on Mgr side Reviewed by:flemenorel
    SID:3041

--- exposure-engine/common/src/main/java/com/alu/e3/common/healthcheck/HealthCheckService.java
+++ exposure-engine/common/src/main/java/com/alu/e3/common/healthcheck/HealthCheckService.java
@@ -18,11 +18,13 @@
  */
 package com.alu.e3.common.healthcheck;
 
+import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
 import java.security.InvalidParameterException;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -34,31 +36,26 @@ import com.alu.e3.common.osgi.api.IHealthCheckFactory;
 import com.alu.e3.data.IHealthCheckService;
 
 /**
- * Health Check class 
+ * Health Check class
  */
 public class HealthCheckService implements IHealthCheckService, Runnable {
 
-	private static final Logger LOG = LoggerFactory.getLogger(HealthCheckService.class);
-	
-	/* final members. */
+	private static final Logger LOG = LoggerFactory .getLogger(HealthCheckService.class);
+
 	private static final String HTTP_RESPONSE_OK = "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nConnection: Close\r\n\r\nHealth check ok.";
 
-	/* members. */
 	private int listeningPort;
 	private ServerSocket serverSocket;
-	
+
 	private AtomicBoolean alive;
 
 	private HealthCheckService() {
 		alive = new AtomicBoolean(false);
 	}
-	
-	/**
-	 * Constructor
-	 */
+
 	public HealthCheckService(String instanceType) {
 		this();
-		
+
 		if (IHealthCheckFactory.GATEWAY_INTERNAL_TYPE.equals(instanceType))
 			this.listeningPort = E3Constant.GATEWAY_INTERNAL_DEFAULT_HEALTH_CHECK_PORT;
 		else if (IHealthCheckFactory.GATEWAY_TYPE.equals(instanceType))
@@ -67,42 +64,40 @@ public class HealthCheckService implements IHealthCheckService, Runnable {
 			this.listeningPort = E3Constant.SPEAKER_DEFAULT_HEALTH_CHECK_PORT;
 		else if (IHealthCheckFactory.MANAGER_TYPE.equals(instanceType))
 			this.listeningPort = E3Constant.MANAGER_DEFAULT_HEALTH_CHECK_PORT;
-		else throw new InvalidParameterException("Invalid instance type.");
+		else
+			throw new InvalidParameterException("Invalid instance type.");
 	}
-	
+
 	/**
-	 * Start healthcheck service
+	 * Start health-check service
 	 */
 	@Override
-	public void start()
-	{
+	public void start() {
 		if (!alive.get()) {
 			try {
 				this.stop();
-				serverSocket = new ServerSocket(listeningPort);	
+				serverSocket = new ServerSocket(listeningPort);
 				alive.set(true);
 				new Thread(this).start();
-		
+
 			} catch (IOException e) {
 				LOG.warn("Problem while starting health check server", e);
 			}
-		}
-		else {
+		} else {
 			LOG.warn("Health check already started");
 		}
 	}
-	
+
 	/**
-	 * Halt healthcheck service
+	 * Halt health-check service
 	 */
 	@Override
-	public void stop()
-	{
+	public void stop() {
 		alive.set(false);
-		
+
 		if (serverSocket == null)
 			return;
-		
+
 		try {
 			serverSocket.close(); // if the service is running, it may make accept() fail.
 		} catch (IOException e) {
@@ -112,63 +107,75 @@ public class HealthCheckService implements IHealthCheckService, Runnable {
 		}
 		LOG.info("Health check stopped");
 	}
-	
+
 	/**
 	 * Server main loop.
 	 */
 	@Override
 	public void run() {
 		while (alive.get()) {
-			
-			/* service not started. */
+
+			// service not started. 
 			if (serverSocket == null || serverSocket.isClosed()) {
-				LOG.info("Health check server thread end");
+				LOG.info("Health check server thread end on port:'{}'",listeningPort);
 				alive.set(false);
 				return;
 			}
 			try {
 				Socket socket = serverSocket.accept();
 				DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-				
+
 				//writeUTF() will produce a leading byte-order character which messes up some clients
 				out.writeChars(HTTP_RESPONSE_OK);
 				out.close();
 				socket.close();
-
-			} catch (SocketException e) {
-				if (LOG.isDebugEnabled()) {
-					// This probably means Manager has closed to communication socket without
-					// listening our answer.
-					LOG.debug("Communication problem", e);
-				}
-			} catch (IOException e) {
-				if (LOG.isDebugEnabled()) {
-					LOG.debug("IO problem", e);
-				}
+				
+			} catch (Exception e) {
+				LOG.error("Server side communication problem on port:'{}' message:'{}'", listeningPort , e.getMessage());
 			}
 		}
 	}
-
 
 	/**
 	 * Check if the health service is alive on the specified host.
 	 */
 	@Override
 	public boolean check(String host) {
-			
-		boolean bCheck = false;
-		
+		Socket clientSocket = null;
+		InputStream in = null;
+		InetSocketAddress endpoint = new InetSocketAddress(host, listeningPort);
+
 		try {
-			Socket clientSocket = new Socket(host, listeningPort);
-			bCheck = clientSocket.isConnected();
-			clientSocket.close();
+			clientSocket = new Socket();
+			clientSocket.connect(endpoint, 2000);
+			in = clientSocket.getInputStream();
+
+			byte buffer[] = new byte[1024];
+
+			if (!LOG.isDebugEnabled()) {
 			
+				while ((in.read(buffer)) != -1);// read to end of stream and drop it...
+
+			} else {
+				ByteArrayOutputStream baos = new ByteArrayOutputStream(HTTP_RESPONSE_OK.length() + 10);
+				for (int s; (s = in.read(buffer)) != -1;) {
+					baos.write(buffer, 0, s);
+				}
+				LOG.debug("GW/Manager:'{}' answer:'{}'", endpoint, baos.toString("UTF-16").replace("\r\n", " ").trim());
+			}
+			return true;
+
 		} catch (Exception e) {
-			if (LOG.isDebugEnabled()) {
-				LOG.debug("Exception while checking health service for " + host, e);
+			LOG.error("host:'{}' not reachable message:'{}'", endpoint, e.getMessage());
+		} finally {
+			try {
+				if (null != clientSocket)
+					clientSocket.close();
+			} catch (IOException e) {
+				// silent
 			}
 		}
-		
-		return bCheck;
+
+		return false;
 	}
 }
